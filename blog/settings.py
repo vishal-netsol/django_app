ae785f4fcf114bd52cd736eb577100c44e538e3f
vishal <vishal.sharma@netsolutions.com>
2017-12-21 17:25:51 +0530

    fab script testing.

--- blog/settings.py
+++ blog/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '9s@+tgy)we+gl0^mso#q!huoj6%ze#g#!57_lvq2tjc7@45kx&'
+# SECRET_KEY = '9s@+tgy)we+gl0^mso#q!huoj6%ze#g#!57_lvq2tjc7@45kx&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- deploy/fabfile.py
+++ deploy/fabfile.py
@@ -1,50 +1,60 @@
 from fabric.contrib.files import append, exists, sed
-from fabric.api import cd, env, local, run
+from fabric.api import cd, env, local, run, settings
 import random
 
-REPO_URL = 'git@github.com:vishal-netsol/django_app.git'  
+REPO_URL = 'git@github.com:vishal-netsol/django_app.git'
 
 def deploy():
-    import pdb; pdb.set_trace()
-    site_folder = f'/home/{env.user}/sites/{env.host}' 
-    run(f'mkdir -p {site_folder}')  
-    with cd(site_folder):  
+    """ Run a command on the host. Assumes user has SSH keys setup """
+    with cd('/Users/pardeepsaini/test'):
         _get_latest_source()
-        _update_settings(env.host)  
-        _update_virtualenv()
-        _update_static_files()
-        _update_database()
+        _update_settings(env.host)
+
+
+# def deploy():
+#     site_folder = f'/home/{env.user}/sites/{env.host}' 
+#     run(f'mkdir -p {site_folder}')  
+#     with cd(site_folder):  
+#         _get_latest_source()
+#         _update_settings(env.host)  
+#         _update_virtualenv()
+#         _update_static_files()
+#         _update_database()
 
 
 def _get_latest_source():
     if exists('.git'):  
-        run('git fetch')  
+        run("git fetch")  
     else:
-        run(f'git clone {REPO_URL} .')  
-    current_commit = local("git log -n 1 --format=%H", capture=True)  
-    run(f'git reset --hard {current_commit}')
+        code_dir = '/Users/pardeepsaini/test/django_app'
+        with settings(warn_only=True):
+            if run("test -d %s" % code_dir).failed:
+                run("git clone %s" % REPO_URL)
+        with cd(code_dir):
+            run("git pull")
+            run("touch app.wsgi")
 
 def _update_settings(site_name):
-    settings_path = 'blog/settings.py'
+    settings_path = '/Users/pardeepsaini/test/django_app/blog/settings.py'
     sed(settings_path, "DEBUG = True", "DEBUG = False")  
     sed(settings_path,
         'ALLOWED_HOSTS =.+$',
-        f'ALLOWED_HOSTS = ["{site_name}"]'  
+        'ALLOWED_HOSTS = [" %s " % site_name]'  
     )
-    secret_key_file = 'superlists/secret_key.py'
+    secret_key_file = '/Users/pardeepsaini/test/django_app/blog/secret_key.py'
     if not exists(secret_key_file):  
         chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
-        key = ''.join(random.SystemRandom().choices(chars, k=50))
-        append(secret_key_file, f'SECRET_KEY = "{key}"')
+        key = ''.join(random.choice(chars) for _ in range(50))
+        append(secret_key_file, 'SECRET_KEY = "%s" % key')
     append(settings_path, '\nfrom .secret_key import SECRET_KEY')
 
-def _update_virtualenv():
-    if not exists('virtualenv/bin/pip'):  
-        run(f'python3.5 -m venv virtualenv')
-    run('./virtualenv/bin/pip install -r requirements.txt')
+# def _update_virtualenv():
+#     if not exists('virtualenv/bin/pip'):  
+#         run(f'python3.5 -m venv virtualenv')
+#     run('./virtualenv/bin/pip install -r requirements.txt')
 
-def _update_static_files():
-    run('./virtualenv/bin/python manage.py collectstatic --noinput')
+# def _update_static_files():
+#     run('./virtualenv/bin/python manage.py collectstatic --noinput')
 
-def _update_database():
-    run('./virtualenv/bin/python manage.py migrate --noinput')
+# def _update_database():
+#     run('./virtualenv/bin/python manage.py migrate --noinput')
